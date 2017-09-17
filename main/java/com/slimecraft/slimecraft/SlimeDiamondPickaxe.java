f676d5359c1fcadaae1bc95208f3bffd5689c50f
takusan23 <32033405+takusan23@users.noreply.github.com>
2017-09-18 00:45:19 +0900

    Hello

--- main/java/com/slimecraft/slimecraft/SlimeDiamondPickaxe.java
+++
@@ -1,9 +0,0 @@
-package com.slimecraft.slimecraft;
-
-import net.minecraft.item.ItemPickaxe;
-
-public class SlimeDiamondPickaxe extends ItemPickaxe{
-	public SlimeDiamondPickaxe(ToolMaterial toolMaterial){
-		super(toolMaterial);
-	}
-}
