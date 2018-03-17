aedad187a0a036a0ccce1bb93a1b013cacac80e5
takusan23 <32033405+takusan23@users.noreply.github.com>
2018-03-18 02:10:55 +0900

    修正

--- 1.10.2/main/java/com/slimecraft/slimecraft/UltimateSlimeDrill.java
+++ 1.10.2/main/java/com/slimecraft/slimecraft/UltimateSlimeDrill.java
@@ -25,12 +25,22 @@ public class UltimateSlimeDrill extends ItemTool {
 	//super(attackDamageIn, attackSpeedIn, materialIn, effectiveBlocksIn);
 		super(10, 20, SlimeCraftItems.UltimateTool, effectiveAgainst);
 	}
+		//private static Set effectiveAgainst = Sets.newHashSet(new Block[] {
+		//});
+
 		private static Set effectiveAgainst = Sets.newHashSet(new Block[] {
+			    Blocks.GRASS, Blocks.DIRT, Blocks.SAND, Blocks.GRAVEL,
+			    Blocks.SNOW_LAYER, Blocks.SNOW, Blocks.CLAY, Blocks.FARMLAND,
+			    Blocks.SOUL_SAND, Blocks.MYCELIUM,Blocks.COBBLESTONE,Blocks.STONE,
+		        Blocks.ACTIVATOR_RAIL, Blocks.COAL_ORE, Blocks.COBBLESTONE, Blocks.DETECTOR_RAIL, Blocks.DIAMOND_BLOCK, Blocks.DIAMOND_ORE, Blocks.DOUBLE_STONE_SLAB, Blocks.GOLDEN_RAIL, Blocks.GOLD_BLOCK, Blocks.GOLD_ORE, Blocks.ICE, Blocks.IRON_BLOCK, Blocks.IRON_ORE, Blocks.LAPIS_BLOCK, Blocks.LAPIS_ORE, Blocks.LIT_REDSTONE_ORE, Blocks.MOSSY_COBBLESTONE, Blocks.NETHERRACK, Blocks.PACKED_ICE, Blocks.RAIL, Blocks.REDSTONE_ORE, Blocks.SANDSTONE, Blocks.RED_SANDSTONE, Blocks.STONE, Blocks.STONE_SLAB, Blocks.STONE_BUTTON, Blocks.STONE_PRESSURE_PLATE,Blocks.IRON_BLOCK,Blocks.OBSIDIAN,
+		        Blocks.PLANKS, Blocks.BOOKSHELF, Blocks.LOG, Blocks.LOG2, Blocks.CHEST, Blocks.PUMPKIN, Blocks.LIT_PUMPKIN, Blocks.MELON_BLOCK, Blocks.LADDER, Blocks.WOODEN_BUTTON, Blocks.WOODEN_PRESSURE_PLATE,Blocks.CRAFTING_TABLE,
+		        SlimeCraftBlocks.SlimeIronBlock,SlimeCraftBlocks.SlimeDiamondBlock,
 		});
 
 
     public EnumActionResult onItemUse(ItemStack stack, EntityPlayer playerIn, World worldIn, BlockPos pos, EnumHand hand, EnumFacing facing, float hitX, float hitY, float hitZ)
 	    {
+
 
         IBlockState iblockstate = worldIn.getBlockState(pos);
         Block block = iblockstate.getBlock();
@@ -54,27 +64,28 @@ public class UltimateSlimeDrill extends ItemTool {
 		        		playerIn.setHeldItem(hand, (new ItemStack(SlimeCraftItems.UltimateSlimeStaff)));
 		        		}
 
-		  		else if(block == Blocks.BEDROCK)
-		  				{
-	        				playerIn.setHeldItem(hand,new ItemStack(SlimeCraftItems.UltimateSlimeStaff));
-	        				--stack.stackSize;
-		  				}
+//		  		else if(block == Blocks.BEDROCK)
+//		  				{
+//	        				playerIn.setHeldItem(hand,new ItemStack(SlimeCraftItems.UltimateSlimeStaff));
+//	        				--stack.stackSize;
+//		  				}
 
 		  		//岩盤以外のブロックを破壊する
-		  		else if(block != Blocks.BEDROCK)
-		        		{
-		        			worldIn.destroyBlock(pos, false);
-		        			TileEntity tileEntity = null;
-		        			block.harvestBlock(worldIn, playerIn, pos, iblockstate, tileEntity, stack);
-		        		}
-
-//		  		//岩盤以外のブロックを破壊する
-//		  		else if(block.getHarvestLevel(iblockstate) <= harv)
+//		  		else if(block != Blocks.BEDROCK)
 //		        		{
 //		        			worldIn.destroyBlock(pos, false);
 //		        			TileEntity tileEntity = null;
 //		        			block.harvestBlock(worldIn, playerIn, pos, iblockstate, tileEntity, stack);
 //		        		}
+
+		  		//岩盤以外のブロックを破壊する
+		  		//setBlockUnbreakableは-1.0Fに設定されている
+		  		else if(block.getBlockHardness(iblockstate, worldIn, pos) != -1.0F)
+		        		{
+		        			worldIn.destroyBlock(pos, false);
+		        			TileEntity tileEntity = null;
+		        			block.harvestBlock(worldIn, playerIn, pos, iblockstate, tileEntity, stack);
+		        		}
 
 	        return EnumActionResult.SUCCESS;
 	    }
